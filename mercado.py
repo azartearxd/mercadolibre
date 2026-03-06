bc6dd510f1b9c0d3d2a65e55935dfdd52136ddfb
azartearxd <adam.calderon.god@gmail.com>
2026-03-05 21:21:13 -0700

    Refactor mercado.py

--- mercado.py
+++ mercado.py
@@ -1,13 +1,9 @@
-# =============================================================================
-# 1️⃣ LIBRERÍAS
-# =============================================================================
+
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-# =============================================================================
-# 2️⃣ OBTENER HTML
-# =============================================================================
+
 url = "https://www.mercadolibre.com.mx/ofertas/novedades-de-temporada"
 
 headers = {
@@ -17,17 +13,13 @@ headers = {
 response = requests.get(url, headers=headers)
 soup = BeautifulSoup(response.text, "html.parser")
 
-# =============================================================================
-# 3️⃣ EXTRAER DATOS
-# =============================================================================
+
 titulos = soup.find_all("a", class_="poly-component__title")
 
-# 👇 precio original (ANTES)
+
 precios = soup.find_all("s", class_="andes-money-amount")
 
-# =============================================================================
-# 4️⃣ GUARDAR EN ARREGLO
-# =============================================================================
+
 productos = []
 
 for i in range(len(titulos)):
@@ -38,16 +30,12 @@ for i in range(len(titulos)):
 
     productos.append([nombre, precio])
 
-# =============================================================================
-# 5️⃣ CONVERTIR A DATAFRAME
-# =============================================================================
+
 df = pd.DataFrame(productos, columns=["Producto", "Precio Antes"])
 
 print(df)
 
-# =============================================================================
-# 6️⃣ GUARDAR CSV
-# =============================================================================
+
 df.to_csv("productos_mercadolibre.csv", index=False, encoding="utf-8")
 
-print("✅ CSV creado: productos_mercadolibre.csv")
+
